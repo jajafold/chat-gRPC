0fd5f8dfe5796e2c5c02fd9de5ed780a883b84d3
Dyrda Lev <akronman0000@gmail.com>
2022-11-18 22:49:41 +0500

    Инициализация

---
+++ GrpcGreeter/Services/GreeterService.cs
@@ -0,0 +1,31 @@
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcGreeter
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public override Task<SimpleMessage> SaySmth(SimpleMessage text, ServerCallContext context)
+        {
+            return Task.FromResult(new SimpleMessage { Message = text.Message + "123123" });
+        }
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeterClient;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest {Name = "GreeterClient"});
+            var reply2 = await client.SaySmthAsync(new SimpleMessage {Message = "123"});
+            Console.WriteLine("Greeting: " + reply2.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
